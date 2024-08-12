67eb241cd5a4642f0c9b803be4e77ce227f5f115
Ge7elias <gesioelias12@gmail.com>
2024-08-11 22:01:03 -0300

    Add files via upload

---
+++ aula00.py
@@ -0,0 +1,14 @@
+Python 3.12.5 (tags/v3.12.5:ff3bc82, Aug  6 2024, 20:45:27) [MSC v.1940 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> nome = input('qual é o seu nome?')
+qual é o seu nome? marcos
+>>> idade = input ('quantos anos você  tem?')
+quantos anos você  tem?25
+>>> anos_para_cem= 100-idade
+100-25
+75
+if anos_ para_cem=75
+>>> print('ola,{marcos}!faltam{75_para_cem}anos para você  completar 100 anos')
+ola,{marcos}!faltam{75_para_cem}anos para você  completar 100 anos
+>>> print('parabens,marcos,você é jovem')
+parabens,marcos,você é jovem
